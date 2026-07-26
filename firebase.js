4418ca8a73f9c784a5cf2a40aa2fb7b61cb66fc0
CODEX045 <gladrobert75@gmail.com>
2026-07-26 05:22:39 +0100

    Use Railway Firebase environment variable

--- firebase.js
+++ firebase.js
@@ -1,6 +1,15 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+    serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+    serviceAccount = (await import("./serviceAccountKey.json", {
+        with: { type: "json" }
+    })).default;
+}
 
 const app = getApps().length
     ? getApps()[0]
